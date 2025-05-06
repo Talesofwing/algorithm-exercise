7d389b008fd5dbc17cdf10f8b4094afae27967fe
zer0 <zerox0103@gmail.com>
2025-05-06 17:24:44 +0900

    Add Recrusive Method to solve EDPC A

--- dp_demos/EDPC/A - Frog1/main.cpp
+++ dp_demos/EDPC/A - Frog1/main.cpp
@@ -7,26 +7,57 @@ using namespace std;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
+int N;
+vector<int> h, dp;
+
+// 貰う
+void method_1() {
+	dp[1] = abs(h[1] - h[0]);
+
+	for (int i = 0; i < N - 1; ++i) {
+		chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+		chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
+	}
+}
+
+// 配る
+void method_2() {
+	for (int i = 0; i < N - 1; ++i) {
+		chmin(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]));
+		chmin(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]));
+	}
+}
+
+// Recursive
+int method_3(int i) {
+	if (dp[i] < INT_MAX) return dp[i];
+
+	if (i == 0) return 0;
+
+	int res = INT_MAX;
+	chmin(res, method_3(i - 1) + abs(h[i] - h[i - 1]));
+	if (i > 1) chmin(res, method_3(i - 2) + abs(h[i] - h[i - 2]));
+
+	return dp[i] = res;
+}
+
 int main() {
 	cin.tie(nullptr);
 	ios::sync_with_stdio(false);
 
-	int N;
 	cin >> N;
 
-	vector<int> h(N);
-	for (int i = 0; i < N; ++i) cin >> h[i];
+	h.resize(N);
+	dp.resize(N);
 
-	vector<int> dp(N);
+	for (int i = 0; i < N; ++i) cin >> h[i];
 	for (int i = 0; i < N; ++i) dp[i] = INT_MAX;
 
 	dp[0] = 0;
-	dp[1] = abs(h[1] - h[0]);
 
-	for (int i = 2; i < N; ++i) {
-		chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
-		chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
-	}
+	// method_1();
+	// method_2();
+	method_3(N - 1);
 
 	cout << dp[N - 1] << endl;
 
